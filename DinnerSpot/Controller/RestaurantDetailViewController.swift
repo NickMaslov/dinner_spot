e9805a501cbe595f088d88fda043e0384a9fb66a
Nick Maslov <kategorychnost@gmail.com>
2021-08-28 00:02:06 -0400

    added mapViews to detailScene and mapViewController'

--- DinnerSpot/Controller/RestaurantDetailViewController.swift
+++ DinnerSpot/Controller/RestaurantDetailViewController.swift
@@ -8,32 +8,33 @@
 import UIKit
 
 class RestaurantDetailViewController: UIViewController {
-        
+    
     @IBOutlet var tableView: UITableView!
     @IBOutlet var headerView: RestaurantDetailHeaderView!
     
     var restaurant: Restaurant = Restaurant()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Configure the navigation bar appearance
         navigationController?.navigationBar.prefersLargeTitles = false
         navigationController?.hidesBarsOnSwipe = false
+        navigationItem.backButtonTitle = ""
         
         tableView.contentInsetAdjustmentBehavior = .never
         
         tableView.delegate = self
         tableView.dataSource = self
         tableView.separatorStyle = .none
-
         
-
+        
+        
         // Configure header view
         headerView.nameLabel.text = restaurant.name
         headerView.typeLabel.text = restaurant.type
         headerView.headerImageView.image = UIImage(named: restaurant.image)
-
+        
         let heartImage = restaurant.isFavorite ? "heart.fill" : "heart"
         headerView.heartButton.tintColor = restaurant.isFavorite ? .systemYellow : .white
         headerView.heartButton.setImage(UIImage(systemName: heartImage), for: .normal)
@@ -46,7 +47,7 @@ class RestaurantDetailViewController: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: true)
         
     }
-
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
@@ -54,35 +55,49 @@ class RestaurantDetailViewController: UIViewController {
 
 extension RestaurantDetailViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        2
+        3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch indexPath.row {
-                case 0:
-                    let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTextCell.self), for: indexPath) as! RestaurantDetailTextCell
-
-                    cell.descriptionLabel.text = restaurant.description
-
-                    return cell
-
-                case 1:
-                    let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTwoColumnCell.self), for: indexPath) as! RestaurantDetailTwoColumnCell
-
-                    cell.column1TitleLabel.text = "Address"
-                    cell.column1TextLabel.text = restaurant.location
-                    cell.column2TitleLabel.text = "Phone"
-                    cell.column2TextLabel.text = restaurant.phone
-
-                    return cell
-
-                default:
-                    fatalError("Failed to instantiate the table view cell for detail view controller")
-
-                }
-            }
-
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTextCell.self), for: indexPath) as! RestaurantDetailTextCell
+            
+            cell.descriptionLabel.text = restaurant.description
+            
+            return cell
+            
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTwoColumnCell.self), for: indexPath) as! RestaurantDetailTwoColumnCell
+            
+            cell.column1TitleLabel.text = "Address"
+            cell.column1TextLabel.text = restaurant.location
+            cell.column2TitleLabel.text = "Phone"
+            cell.column2TextLabel.text = restaurant.phone
+            
+            return cell
+            
+        case 2:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailMapCell.self), for: indexPath) as! RestaurantDetailMapCell
+            cell.configure(location: restaurant.location)
+            cell.selectionStyle = .none
+            
+            return cell
+            
+        default:
+            fatalError("Failed to instantiate the table view cell for detail view controller")
+            
+        }
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showMap" {
+                let destinationController = segue.destination as! MapViewController
+                destinationController.restaurant = restaurant
+            }
+    }
+    
+}
+
 
 
